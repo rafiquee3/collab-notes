eaa2ab4edb57e9cfefbb560f4e4b29458ee6f0a0
Rafal <rafiquee.code@gmail.com>
2026-03-27 21:26:23 +0100

    fix: resolve WebSocket naming conflict

--- src/server/wss.ts
+++ src/server/wss.ts
@@ -1,19 +1,19 @@
-const WebSocket = require('ws');
-const http = require('http');
-const setupWSConnection = require('y-websocket/bin/utils').setupWSConnection;
+import { WebSocketServer } from "ws";
+import http from "http";
+const { setupWSConnection } = require("y-websocket/bin/utils");
 
 const port = 1234;
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Yjs WebSocket Server is running');
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Yjs WebSocket Server is running");
 });
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
 
-wss.on('connection', (conn, req) => {
+wss.on("connection", (conn, req) => {
   setupWSConnection(conn, req);
-  console.log('New connection to room:', req.url);
+  console.log("New connection to room:", req.url);
 });
 
 server.listen(port, () => {
